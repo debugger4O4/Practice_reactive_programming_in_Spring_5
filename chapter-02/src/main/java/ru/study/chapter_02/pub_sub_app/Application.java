5acdce1b749ff89b6c2b9aa3e1a214488138dd54
Kirill <seeraydary@gmail.com>
2024-10-08 14:24:39 +0300

    chapter-02

---
+++ chapter-02/src/main/java/ru/study/chapter_02/pub_sub_app/Application.java
@@ -0,0 +1,44 @@
+package ru.study.chapter_02.pub_sub_app;
+
+import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.aop.interceptor.SimpleAsyncUncaughtExceptionHandler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+/**
+ * Настройка поддержки асинхронного выполнения
+ */
+@EnableAsync
+@SpringBootApplication
+public class Application implements AsyncConfigurer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    // Executor для асинхронной обработки
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setThreadNamePrefix("sse-");
+        // Количество основных потоков
+        executor.setCorePoolSize(2);
+        // Маскимальное количество потоков
+        executor.setMaxPoolSize(100);
+        // Емкость очереди потоков
+        executor.setQueueCapacity(5);
+        executor.initialize();
+        return executor;
+    }
+
+    @Override
+    public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
+        // Обработчик исключений в процессе асинхронного выполнения
+        return new SimpleAsyncUncaughtExceptionHandler();
+    }
+}
