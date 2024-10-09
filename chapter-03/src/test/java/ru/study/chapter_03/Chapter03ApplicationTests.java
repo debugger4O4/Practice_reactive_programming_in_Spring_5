29ee8f9015ab666716753f583791ce4ad8b51ede
Kirill <seeraydary@gmail.com>
2024-10-09 11:13:02 +0300

    chapter-03

---
+++ chapter-03/src/test/java/ru/study/chapter_03/Chapter03ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.study.chapter_03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter03ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
