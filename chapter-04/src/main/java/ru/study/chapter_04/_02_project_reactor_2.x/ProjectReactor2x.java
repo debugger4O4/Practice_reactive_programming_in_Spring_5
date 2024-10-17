4966b98b543cd3c3d07ae473a612848171a5bdb9
Kirill <seeraydary@gmail.com>
2024-10-17 12:02:42 +0300

    chapter_04._02_project_reactor_2.x

---
+++ chapter-04/src/main/java/ru/study/chapter_04/_02_project_reactor_2.x/ProjectReactor2x.java
@@ -0,0 +1,26 @@
+package ru.study.chapter_04._02_project_reactor_2.x;
+
+/**
+ * Project Reactor 2.x.
+ */
+public class ProjectReactor2x {
+
+//    stream()
+          /*
+          retry() - увеличение устойчивости процесса обработки, т.е. когда в потоке возникает ошибка, операции в нем
+          повторяются.
+           */
+//        .retry()
+          /*
+          onOverflowBuffer() и onOverflowDrop() добавляют управление обратным давлением для случая, когда издатель
+          поддерживает только модель PUSH(и не учитывается спрос потребителя).
+           */
+//        .onOverflowBuffer()
+//        .onOverflowDrop()
+          /*
+          dispatchOn() - выделение нового диспетчера Dispatcher для работы с этим реактивным потоком данных. Т.о.
+          обеспечивается возможность асинхронной обработки сообщений.
+           */
+//        .dispatchOn(new RingBufferDispatcher("test"))
+
+}
