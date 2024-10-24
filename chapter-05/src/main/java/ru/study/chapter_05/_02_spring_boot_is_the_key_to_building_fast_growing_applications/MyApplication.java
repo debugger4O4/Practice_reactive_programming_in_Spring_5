9f8c2a8d21fb01120858729ed5110fb6e81a1f84
Kirill <seeraydary@gmail.com>
2024-10-24 18:28:18 +0300

    chapter_05._02_spring_boot_is_the_key_to_building_fast_growing_applications

---
+++ chapter-05/src/main/java/ru/study/chapter_05/_02_spring_boot_is_the_key_to_building_fast_growing_applications/MyApplication.java
@@ -0,0 +1,14 @@
+package ru.study.chapter_05._02_spring_boot_is_the_key_to_building_fast_growing_applications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Spring Boot как ключи к созданию быстро растущих приложений.
+ */
+@SpringBootApplication
+public class MyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+}
