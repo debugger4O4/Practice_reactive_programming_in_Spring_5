b2df56aec726ead0f701a9644ae770486992e649
Kirill <seeraydary@gmail.com>
2024-10-24 10:26:44 +0300

    chapter_05

---
+++ chapter-05/src/main/java/ru/study/chapter_05/Chapter05Application.java
@@ -0,0 +1,13 @@
+package ru.study.chapter_05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter05Application.class, args);
+    }
+
+}
---
+++ chapter-05/src/test/java/ru/study/chapter_05/Chapter05ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.study.chapter_05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
