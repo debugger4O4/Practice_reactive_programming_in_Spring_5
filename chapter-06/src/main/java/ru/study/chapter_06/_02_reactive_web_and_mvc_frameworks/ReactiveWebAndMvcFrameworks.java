223ba93089d7b90920097dc58c6086c6b631ed33
Kirill <seeraydary@gmail.com>
2024-10-25 14:40:10 +0300

    chapter_06._02_reactive_web_and_mvc_frameworks

---
+++ chapter-06/src/main/java/ru/study/chapter_06/_02_reactive_web_and_mvc_frameworks/ReactiveWebAndMvcFrameworks.java
@@ -0,0 +1,28 @@
+package ru.study.chapter_06._02_reactive_web_and_mvc_frameworks;
+
+/**
+ * Реактивные фреймворки Web и MVC.
+ */
+public class ReactiveWebAndMvcFrameworks {
+    // Реактивный интерфейс.
+//    interface HandlerMapping {
+        // Старая реалзация.
+        /* HandlerExecutionChain getHandler(HttpServletRequest request); */
+        // Новая реализация.
+//        Mono<Object> getHandler(ServerWebExchange exchange);
+//    }
+    // Реактивная версия.
+//    interface HandlerAdapter {
+//        boolean supports(Object handler);
+    // Старая реалзация.
+//        /* ModelAndView handle(
+//                HttpServletRequest request, HttpServletResponse response,
+//                Object handler); */
+    // Новая реализация.
+        // ServerWebExchange объединяет экземпляры запроса и ответа.
+//        Mono<HandlerResult> handle(
+//                ServerWebExchange exchange,
+//                Object handler
+//        );
+//    }
+}
