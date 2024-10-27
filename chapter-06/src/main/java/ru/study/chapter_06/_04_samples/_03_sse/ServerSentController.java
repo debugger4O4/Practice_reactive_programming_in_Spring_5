9a52a958735d471a16d804b9850240f022d2b9f6
Kirill <seeraydary@gmail.com>
2024-10-27 12:29:05 +0300

    chapter_06._04_samples._03_sse

---
+++ chapter-06/src/main/java/ru/study/chapter_06/_04_samples/_03_sse/ServerSentController.java
@@ -0,0 +1,45 @@
+package ru.study.chapter_06._04_samples._03_sse;
+
+//import java.util.Map;
+//
+//import reactor.core.publisher.Flux;
+//
+//import org.springframework.http.codec.ServerSentEvent;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+/**
+ * Реактивный поток SSE(Server-Sent Event) и легковесная замена WebSocket.
+ */
+//@RestController
+//public class ServerSentController {
+//
+//    private Map<String, StocksService> stringStocksServiceMap;
+//
+//    @GetMapping("/sse/stocks")
+//    public Flux<ServerSentEvent<?>> streamStocks() {
+//        return Flux
+//                .fromIterable(stringStocksServiceMap.values())
+                // Объединение всех доступных источников информации и передача их клиенту.
+//                .flatMap(StocksService::stream)
+                // Преобразование каждого StockItem в ServerSentEvent.
+//                .<ServerSentEvent<?>>map(item ->
+                        // Передача билдеру id и название события для настройки экземпляра.
+//                        ServerSentEvent
+//                                .builder(item)
+//                                .event("StockItem")
+//                                .id(item.getId())
+//                                .build()
+//                )
+                /*
+                 Запуск потока данных сконкретным экземпляром ServerSentEvent, который объявляет клиенту доступные
+                 каналы информации.
+                 */
+//                .startWith(
+//                        ServerSentEvent
+//                                .builder()
+//                                .event("Stocks")
+//                                .data(stringStocksServiceMap.keySet())
+//                                .build()
+//                );
+//    }
+//}
---
+++ chapter-06/src/main/java/ru/study/chapter_06/_04_samples/_03_sse/StockItem.java
@@ -0,0 +1,15 @@
+package ru.study.chapter_06._04_samples._03_sse;
+
+public class StockItem {
+
+    private String id;
+    private String type;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
