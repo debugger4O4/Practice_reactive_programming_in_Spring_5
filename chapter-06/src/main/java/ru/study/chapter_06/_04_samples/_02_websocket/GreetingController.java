bc1251ea5d3958ac35993261051f465c65cd835c
Kirill <seeraydary@gmail.com>
2024-10-27 12:11:29 +0300

    chapter_06._04_samples._02_websocket

--- chapter-06/src/main/java/ru/study/chapter_06/_04_samples/_02_websocket/GreetingController.java
+++ chapter-06/src/main/java/ru/study/chapter_06/_04_samples/_02_websocket/GreetingController.java
@@ -4,6 +4,15 @@ import org.springframework.stereotype.Controller;
 
 /**
  * Сравнение WebFlux WebSocket и Spring WebSocket.
+ * <br>
+ * Разница между WebFlux WebSocket и Spring WebSocket заключается в некоторых особенностях:
+ * Spring WebSocket блокирует взаимодействие с IO, в то время как Spring WebFlux предлагает полностью неблокирующую
+ * запись и чтение.
+ * Модуль WebFlux обеспечивает лучшую потоковую абстракцию, используя спецификацию Reactive Streams и проект Reactor.
+ * В то время как интерфейс WebSocketHandler из старого модуля WebSocket позволяет обрабатывать только одно сообщение
+ * за раз.
+ * Кроме того, Spring WebFlux WebSocket построен на основе проекта Reactor Netty, который является высокопроизводительной
+ * платформой для сетевых приложений
  */
 @Controller
 public class GreetingController {
