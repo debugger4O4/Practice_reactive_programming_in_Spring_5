4bfecf829abdec8391f26c6c768e424859e72624
Kirill <seeraydary@gmail.com>
2024-10-27 12:02:28 +0300

    chapter_06._04_samples._02_websocket

---
+++ chapter-06/src/main/java/ru/study/chapter_06/_04_samples/_02_websocket/WebSocketClient.java
@@ -0,0 +1,16 @@
+package ru.study.chapter_06._04_samples._02_websocket;
+
+/**
+ * Интерфейс для создания соединения с WebSocket со стороны клиента.
+ */
+//public interface WebSocketClient {
+//    Mono<Void> execute(
+//            URL url,
+//            WebSocketHandler handler
+//    );
+//    Mono<Void> execute(
+//            URL url,
+//            HttpHeaders header,
+//            WebSocketHandler handler
+//    );
+//}
