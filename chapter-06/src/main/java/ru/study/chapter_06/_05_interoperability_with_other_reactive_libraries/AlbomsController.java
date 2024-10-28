5d9a66e5fbbaec6149309e2f6be6a8529203c60c
Kirill <seeraydary@gmail.com>
2024-10-28 15:49:40 +0300

    chapter_06._05_interoperability_with_other_reactive_libraries

---
+++ chapter-06/src/main/java/ru/study/chapter_06/_05_interoperability_with_other_reactive_libraries/AlbomsController.java
@@ -0,0 +1,24 @@
+package ru.study.chapter_06._05_interoperability_with_other_reactive_libraries;
+
+//import io.reactivex.Observable;
+//...
+//
+/**
+ * Взаимодействие с другими реактивными библиотеками.
+ */
+//@RestController
+//public class AlbomsController {
+//    final ReactiveAdapterRegistry adapterRegistry;
+//    ...
+    // Обработка запросов.
+//    @GetMapping("/songs")
+//    public Observable<Song> findAlbomByArtists(Flux<Artist> artistFlux) {
+//        Observable<Artist> observable = adapterRegistry
+//                .getAdapter(Observable.class)
+//                .fromPublisher(artistFlux);
+        // Преобразование artistFlux в Observable<Artists> и выполнение бизнес-логики.
+//        Observable<Song> albomsObservable = ...;
+//
+//        return albomsObservable;
+//    }
+//}
