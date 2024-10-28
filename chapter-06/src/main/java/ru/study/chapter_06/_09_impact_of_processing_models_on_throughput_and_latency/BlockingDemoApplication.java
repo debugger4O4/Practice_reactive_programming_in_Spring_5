f0b2b36f389456b8b93cc548457cd7fa218be792
Kirill <seeraydary@gmail.com>
2024-10-28 18:34:54 +0300

    chapter_06._09_impact_of_processing_models_on_throughput_and_latency

---
+++ chapter-06/src/main/java/ru/study/chapter_06/_09_impact_of_processing_models_on_throughput_and_latency/BlockingDemoApplication.java
@@ -0,0 +1,18 @@
+package ru.study.chapter_06._09_impact_of_processing_models_on_throughput_and_latency;
+
+/**
+ * Влияние моделей обработки на пропускную способность и задержку.
+ */
+//@RestController
+//@SpringBootApplication
+//public class BlockingDemoApplication implements InitializingBean {
+//
+//    ...
+    // Посылка двух запросов удаленному серверу.
+//    @GetMapping("/")
+//    public void get() {
+//       restTemplate.getForObject(someUri, String.class);
+//       restTemplate.getForObject(someUri, String.class);
+//    }
+//    ...
+//}
---
+++ chapter-06/src/main/java/ru/study/chapter_06/_09_impact_of_processing_models_on_throughput_and_latency/ReactiveDemoApplication.java
@@ -0,0 +1,33 @@
+package ru.study.chapter_06._09_impact_of_processing_models_on_throughput_and_latency;
+
+/**
+ * Влияние моделей обработки на пропускную способность и задержку.
+ */
+//@RestControlller
+//@SpringBootApplication
+//public class ReactiveDemoApplication implements InitializingBean {
+//    ...
+//    @GetMapping("/")
+//    public Mono<Void> get() {
+//        return
+//                webClient
+                        // Удаленный вызов.
+//                        .get()
+//                        .uti(someUri)
+//                        .retrieve()
+//                        .bodyToMono(DataBuffer.class)
+//                        .doOnNext(DataBufferUtils::release)
+//                        .then(
+//                                webClient
+                                        // Удаленный вызов.
+//                                        .get()
+//                                        .uti(someUri)
+//                                        .retrieve()
+//                                        .bodyToMono(DataBuffer.class)
+//                                        .doOnNext(DataBufferUtils::release)
+//                                        .then()
+//                        )
+//                        .then();
+//    }
+//    ...
+//}
