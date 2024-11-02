519c0ab852d48aacd608bdc4a06e4fefb02c1f17
Kirill <seeraydary@gmail.com>
2024-11-02 15:35:38 +0300

    chapter_07._01_jdbc

---
+++ chapter-07/src/main/java/ru/study/chapter_07/_01_jdbc/Book.java
@@ -0,0 +1,17 @@
+package ru.study.chapter_07._01_jdbc;
+
+//import lombok.AllArgsConstructor;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import org.springframework.data.annotation.Id;
+/**
+ * Коллекция книг.
+ */
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//public class Book {
+//    @Id
+//    private int id;
+//    private String title;
+//}
