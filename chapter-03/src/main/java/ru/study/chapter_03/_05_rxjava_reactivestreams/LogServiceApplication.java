814249d9784fb29d10d2feea35e8ae8a3d93d992
Kirill <seeraydary@gmail.com>
2024-11-02 15:51:38 +0300

    chapter_03. Uncommented.

--- chapter-03/src/main/java/ru/study/chapter_03/_05_rxjava_reactivestreams/LogServiceApplication.java
+++ chapter-03/src/main/java/ru/study/chapter_03/_05_rxjava_reactivestreams/LogServiceApplication.java
@@ -1,41 +1,36 @@
 package ru.study.chapter_03._05_rxjava_reactivestreams;
-/*
-Косяк с импортами
- */
-//import java.io.File;
-//import java.util.Objects;
-//import java.util.concurrent.TimeUnit;
-//
-//import io.reactivex.Flowable;
-//import org.reactivestreams.Publisher;
-//import ratpack.func.Function;
-//import ratpack.server.RatpackServer;
-//import ratpack.sse.ServerSentEvents;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.ConfigurableApplicationContext;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-//
-//import static ratpack.sse.ServerSentEvents.serverSentEvents;
-//
-//@SpringBootApplication
-//@RestController
-//public class LogServiceApplication {
-//
-//    public static void main(String[] args) throws Exception {
-//        ConfigurableApplicationContext context =
-//                SpringApplication.run(LogServiceApplication.class, args);
-//
-//        LogService logsService = context.getBean(LogService.class);
-//        FileService fileService = context.getBean(FileService.class);
-//
-//        File tempFile = File.createTempFile("LogServiceApplication", ".tmp");
-//        tempFile.deleteOnExit();
-//
-//        fileService.writeTo(tempFile.getAbsolutePath(), logsService.stream());
+
+import java.io.File;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import io.reactivex.Flowable;
+import org.reactivestreams.Publisher;
+import ratpack.func.Function;
+import ratpack.server.RatpackServer;
+import ratpack.sse.ServerSentEvents;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import static ratpack.sse.ServerSentEvents.serverSentEvents;
+
+@SpringBootApplication
+@RestController
+public class LogServiceApplication {
+
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext context =
+                SpringApplication.run(LogServiceApplication.class, args);
+
+        LogService logsService = context.getBean(LogService.class);
+        FileService fileService = context.getBean(FileService.class);
+
+        File tempFile = File.createTempFile("LogServiceApplication", ".tmp");
+        tempFile.deleteOnExit();
+
+        fileService.writeTo(tempFile.getAbsolutePath(), logsService.stream());
          /**
           * Усовершенствования в Ratpack
           * Ratpack — это легковесный фреймворк для разработки веб-приложений
@@ -51,39 +46,39 @@ package ru.study.chapter_03._05_rxjava_reactivestreams;
           * — поддержка функций непосредственно в коде приложения
           */
           // Начальное действие сервера и объявление обработчика запроса
-//        RatpackServer.start(server ->
-//                server.handlers(chain ->
-//                        chain.all(ctx -> {
+        RatpackServer.start(server ->
+                server.handlers(chain ->
+                        chain.all(ctx -> {
                               // Объявление потока записей для журналирования
-//                            Publisher<String> logs = logsService.stream();
+                            Publisher<String> logs = logsService.stream();
                               /*
                               Подготовка ServerSentEvents. Используется на этапе отображения для преобразования
                               элемнетов, присылаемых издателем Publisher в представление ServerSentEvents
                                */
-//                            ServerSentEvents events = serverSentEvents(
-//                                    logs,
-//                                    event -> event.id(Objects::toString)
-//                                            .event("log")
-//                                            .data(Function.identity())
-//                            );
+                            ServerSentEvents events = serverSentEvents(
+                                    logs,
+                                    event -> event.id(Objects::toString)
+                                            .event("log")
+                                            .data(Function.identity())
+                            );
                               // Отображение потока данных
-//                            ctx.render(events);
-//                        })
-//                )
-//        );
-//    }
-//
-//    @RequestMapping("/")
-//    public String root() {
-//        return "Please go to http://localhost:8080/logs";
-//    }
-//
-//    @RequestMapping("/logs")
-//    public SseEmitter mockLogs() {
-//        SseEmitter emitter = new SseEmitter();
-//        Flowable.interval(300, TimeUnit.MILLISECONDS)
-//                .map(i -> "[" + System.nanoTime() + "] [LogServiceApplication] [Thread " + Thread.currentThread() + "] Some loge here " + i + "\n")
-//                .subscribe(emitter::send, emitter::completeWithError, emitter::complete);
-//        return emitter;
-//    }
-//}
+                            ctx.render(events);
+                        })
+                )
+        );
+    }
+
+    @RequestMapping("/")
+    public String root() {
+        return "Please go to http://localhost:8080/logs";
+    }
+
+    @RequestMapping("/logs")
+    public SseEmitter mockLogs() {
+        SseEmitter emitter = new SseEmitter();
+        Flowable.interval(300, TimeUnit.MILLISECONDS)
+                .map(i -> "[" + System.nanoTime() + "] [LogServiceApplication] [Thread " + Thread.currentThread() + "] Some loge here " + i + "\n")
+                .subscribe(emitter::send, emitter::completeWithError, emitter::complete);
+        return emitter;
+    }
+}
--- chapter-03/src/main/java/ru/study/chapter_03/_06_vert.x/VertxDemoApp.java
+++ chapter-03/src/main/java/ru/study/chapter_03/_06_vert.x/VertxDemoApp.java
@@ -1,14 +1,11 @@
 package ru.study.chapter_03._06_vert.x;
 
-/*
-Косяк с импортами
- */
-//import io.reactivex.rxjava3.core.Flowable;
-//import io.vertx.core.AbstractVerticle;
-//import io.vertx.core.Launcher;
-//import io.vertx.core.buffer.Buffer;
-//import io.vertx.core.http.HttpServerResponse;
-//import io.vertx.core.streams.Pump;
+import io.reactivex.rxjava3.core.Flowable;
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Launcher;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.streams.Pump;
 
 /**
  * Изменения в Vert.x
@@ -19,41 +16,41 @@ package ru.study.chapter_03._06_vert.x;
  * В январе 2016 года Tim Fox покинул проект Vert.x, и его заменил Julien Viet. Vert.x используется в различных
  * проектах, таких как разработка приложений для Интернета вещей (IoT) и корпоративных сервисов
  */
-//public class VertxDemoApp extends AbstractVerticle {
-//
-//    @Override
-//    public void start() {
-//        LogService logsService = new MockLogService();
-//        vertx.createHttpServer()
+public class VertxDemoApp extends AbstractVerticle {
+
+    @Override
+    public void start() {
+        LogService logsService = new MockLogService();
+        vertx.createHttpServer()
                   /*
                    Объявление обработчика запросов. Обобщенный обработчик, способен обрабатывать любые запросы,
                    отправленные серверу
                    */
-//                .requestHandler(request -> {
+                .requestHandler(request -> {
                       /*
                       Объявление подписчика Subscriber и HTTP- ответа. ReactiveReadStream реализует оба интерфейса -
                       - org.reactivestreams.Subscriber и ReadStreams, что позволяет преобразовать любую реализацию
                       Publisher в источник данных, совместимый с Vert.x API
                        */
-//                    ReactiveReadStream<Buffer> rrs = ReactiveReadStream.readStream();
-//                    HttpServerResponse response = request.response();
+                    ReactiveReadStream<Buffer> rrs = ReactiveReadStream.readStream();
+                    HttpServerResponse response = request.response();
                       /*
                       Объявление потока обработки. Тут идет ссылка на новый интерфейс LogService, основанный на Reactive
                       Streams, и для преобразования элементов из потока данных используем Flowable API и RxJava 2.x.
                        */
-//                    Flowable<Buffer> logs = Flowable.fromPublisher(logsService.stream())
-//                            .map(Buffer::buffer)
-//                            .doOnTerminate(response::end);
+                    Flowable<Buffer> logs = Flowable.fromPublisher(logsService.stream())
+                            .map(Buffer::buffer)
+                            .doOnTerminate(response::end);
                       /*
                       Этап формирования подписки. После объявления потока обработки можно подписать ReactiveReadStream
                       на Flowable
                        */
-//                    logs.subscribe(rrs);
+                    logs.subscribe(rrs);
                       // Этап подготовки ответа
-//                    response.setStatusCode(200);
-//                    response.setChunked(true);
-//                    response.putHeader("Content-Type", "text/plain");
-//                    response.putHeader("Connection", "keep-alive");
+                    response.setStatusCode(200);
+                    response.setChunked(true);
+                    response.putHeader("Content-Type", "text/plain");
+                    response.putHeader("Connection", "keep-alive");
                       /*
                       Отправка окончательного ответа клиенту. Pump играет важную роль в сложном механизме управления
                       обратным давлением для предотвращения переполнения буфера WriteStream
@@ -68,14 +65,14 @@ package ru.study.chapter_03._06_vert.x;
                       любого ReadStream в любой WriteStream, например, из HttpServerRequest в AsyncFile или из
                       NetSocket в WebSocket
                        */
-//                    Pump.pump(rrs, response)
-//                            .start();
-//                })
-//                .listen(8080);
-//        System.out.println("HTTP server started on port 8080");
-//    }
-//
-//    public static void main(final String[] args) {
-//        Launcher.executeCommand("run", VertxDemoApp.class.getName());
-//    }
-//}
+                    Pump.pump(rrs, response)
+                            .start();
+                })
+                .listen(8080);
+        System.out.println("HTTP server started on port 8080");
+    }
+
+    public static void main(final String[] args) {
+        Launcher.executeCommand("run", VertxDemoApp.class.getName());
+    }
+}
