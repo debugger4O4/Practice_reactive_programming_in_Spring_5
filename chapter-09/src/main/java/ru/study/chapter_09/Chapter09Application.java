f3731b3f186f1fbfe042f18be9e4256b3caf6146
debugger4O4 <now-kirill@ya.ru>
2024-12-20 11:19:45 +0300

    Update class name.

--- chapter-09/src/main/java/ru/study/chapter_09/Chapter09Application.java
+++ chapter-09/src/main/java/ru/study/chapter_09/Chapter09Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Chapter07Application {
+public class Chapter09Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Chapter07Application.class, args);
