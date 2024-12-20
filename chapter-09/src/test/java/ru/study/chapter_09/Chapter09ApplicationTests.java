8ed125b58bde0834be2c6c93026841008185f7b8
debugger4O4 <now-kirill@ya.ru>
2024-12-20 11:13:18 +0300

    Start chapter-09 and update comments.

--- chapter-09/src/test/java/ru/study/chapter_09/Chapter09ApplicationTests.java
+++ chapter-09/src/test/java/ru/study/chapter_09/Chapter09ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Chapter07ApplicationTests {
+class Chapter09ApplicationTests {
 
     @Test
     void contextLoads() {
