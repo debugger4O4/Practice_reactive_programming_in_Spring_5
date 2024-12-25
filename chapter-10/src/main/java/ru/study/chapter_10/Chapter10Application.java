d6f8f02467832692560f6a06a07d553b55e79e4b
debugger4O4 <now-kirill@ya.ru>
2024-12-25 16:09:37 +0300

    chapter_10

---
+++ chapter-10/src/main/java/ru/study/chapter_10/Chapter10Application.java
@@ -0,0 +1,13 @@
+package ru.study.chapter_09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter09Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter07Application.class, args);
+    }
+
+}
