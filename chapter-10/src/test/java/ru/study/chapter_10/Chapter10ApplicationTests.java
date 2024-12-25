94712d7b5aac8790f80c86eb0364bff11f039d2d
debugger4O4 <now-kirill@ya.ru>
2024-12-25 16:08:43 +0300

    chapter_10

---
+++ chapter-10/src/test/java/ru/study/chapter_10/Chapter10ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.study.chapter_09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chapter09ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
